7a982d0eb76e5bd92f4e30e59641c12ed90fbfaa
Hannes Hornwall <hannes@hornwall.me>
2018-04-08 16:52:10 +0200

    I3 + pollbar setup

---
+++ config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar top -r &
+
+echo "Bars launched..."
