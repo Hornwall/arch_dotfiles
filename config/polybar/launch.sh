42ad359a4b124a2e84474ef4002cff2e0a15652d
Hannes Hornwall <hannes@hornwall.me>
2018-04-09 16:50:37 +0200

    Add multimonitor support and gnome keyring

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,6 +6,12 @@ killall -q polybar
 # wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar top -r &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -r top &
+  done
+else
+  polybar -r top &
+fi
 
 echo "Bars launched..."
