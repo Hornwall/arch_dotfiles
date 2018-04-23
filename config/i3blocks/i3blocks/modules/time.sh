8c1ca3538e07b20a95fb39f21b318d71aec0e195
Hannes Hornwall <hannes@hornwall.me>
2018-04-23 23:13:41 +0200

    Update bar scripts

---
+++ config/i3blocks/i3blocks/modules/time.sh
@@ -0,0 +1,4 @@
+background='#d1434c'
+nextbg='#e37c44'
+date=`date '+%Y-%m-%d %H:%M'`
+echo "<span background='$nextbg' foreground='$background'></span><span background='$background'>$date </span>"
