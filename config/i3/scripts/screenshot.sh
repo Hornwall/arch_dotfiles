56c6628319b7e24223f0cbd6f8aa679afab7e46f
Hannes Hornwall <hannes@hornwall.me>
2018-07-04 16:55:17 +0200

    Add screenshot notification

--- config/i3/scripts/screenshot.sh
+++ config/i3/scripts/screenshot.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 scrot -s ~/foo.png && xclip -selection clipboard -t image/png -i ~/foo.png && rm ~/foo.png
+if [ $? -eq 0 ]; then
+  notify-send Screenshot "Saved to clipboard" -t 2000
+fi
