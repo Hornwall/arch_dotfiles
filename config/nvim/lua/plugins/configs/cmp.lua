818b6afa25f5c35b235b43ec07cc740a767b473b
Hannes Hornwall <hannes@hornwall.me>
2022-03-21 16:09:24 +0100

    Only activate completion on tab

--- config/nvim/lua/plugins/configs/cmp.lua
+++ config/nvim/lua/plugins/configs/cmp.lua
@@ -1,5 +1,10 @@
-print("LOADING CMP")
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 vim.opt.completeopt = {
    "menu",
@@ -13,7 +18,31 @@ cmp.setup({
      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
    end,
  },
+ completion = {
+   autocomplete = false,
+ },
  mapping = {
+   ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
