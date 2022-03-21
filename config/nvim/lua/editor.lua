5278bff2a4c48ddd7b67a638bd516607fa82e1f6
Hannes Hornwall <hannes@hornwall.me>
2022-03-21 09:30:43 +0100

    Enable global status line

--- config/nvim/lua/editor.lua
+++ config/nvim/lua/editor.lua
@@ -38,6 +38,9 @@ opt.colorcolumn = "80"
 -- Do not wrap and color column 80
 opt.wrap        = false
 
+-- Global status line
+opt.laststatus = 3
+
 local vimrc_ex_group = vim.api.nvim_create_augroup("vimrcEx", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "text",
