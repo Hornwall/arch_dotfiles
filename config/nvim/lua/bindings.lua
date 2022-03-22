d9864b0edad82481522e065eb7238f0f3396c04b
Hannes Hornwall <hannes@hornwall.me>
2022-03-22 13:42:57 +0100

    Use new keymap api

--- config/nvim/lua/bindings.lua
+++ config/nvim/lua/bindings.lua
@@ -1,36 +1,28 @@
-function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
-local opt = {}
+local keymap = vim.keymap
 
 -- Bind splitting
-map("n", "<leader>h", ":rightbelow split<CR>")
-map("n", "<leader>v", ":rightbelow vsplit<CR>")
+keymap.set("n", "<leader>h", "<cmd>rightbelow split<CR>")
+keymap.set("n", "<leader>v", "<cmd>rightbelow vsplit<CR>")
 
-map("n", "<leader>cs", ":source $MYVIMRC<CR>")
+keymap.set("n", "<leader>cs", "<cmd>source $MYVIMRC<CR>")
 
 -- nvimtree
-map("n", "<C-n>", ":NvimTreeToggle<CR>", {silent = true})
+keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", {silent = true})
 
-map("n", "<C-P>", "<cmd>Telescope find_files<CR>")
-map("n", "<C-S>", "<cmd>Telescope live_grep<CR>")
+keymap.set("n", "<C-P>", "<cmd>Telescope find_files<CR>")
+keymap.set("n", "<C-S>", "<cmd>Telescope live_grep<CR>")
 
 -- vimtest
-map("n", "<leader>s", ":TestNearest<CR>")
-map("n", "<leader>t", ":TestFile<CR>")
-map("n", "<leader>a", ":TestSuite<CR>")
-map("n", "<leader>l", ":TestLast<CR>")
+keymap.set("n", "<leader>s", "<cmd>TestNearest<CR>")
+keymap.set("n", "<leader>t", "<cmd>TestFile<CR>")
+keymap.set("n", "<leader>a", "<cmd>TestSuite<CR>")
+keymap.set("n", "<leader>l", "<cmd>TestLast<CR>")
 
 -- Git
-map("n", "<leader>gpr", [[<Cmd> Dispatch git p-r -p <CR>]])
+keymap.set("n", "<leader>gpr", "<Cmd> Dispatch git p-r -p <CR>")
 
 -- vim-argwrap
-map("n", "<leader>w", ":ArgWrap<CR>")
+keymap.set("n", "<leader>w", "<cmd>ArgWrap<CR>")
 
 -- gitmoji
-map("n", "<leader>j", [[<Cmd> read !gitmoji-selector<CR>]])
+keymap.set("n", "<leader>j", "<cmd> read !gitmoji-selector<CR>")
