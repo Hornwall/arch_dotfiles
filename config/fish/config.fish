8f9a24122c4521ed56fa5074a199fac2694e9ebf
Hannes Hornwall <hannes@hornwall.me>
2022-10-02 20:57:25 +0200

    Add fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,32 @@
+set fish_greeting
+
+set -x EDITOR "nvim"
+set -x CDPATH "~/code" "~/"
+
+function fish_prompt -d "Write out the prompt"
+  set_color cyan
+  printf "%s %s" (prompt_pwd) '$ '
+  set_color normal
+end
+
+function fish_right_prompt
+  set_color green
+  echo (fish_git_prompt)
+  set_color normal
+end
+
+fish_add_path "/home/hannes/.yarn/bin"
+fish_add_path "~/.rbenv/bin"
+fish_add_path "./bin"
+
+alias vim=nvim
+alias :q=exit
+alias git=hub
+alias rs="rails s"
+alias rc="rails c"
+
+eval "$(rbenv init -)"
+
+function nvm
+    bass source /usr/share/nvm/init-nvm.sh
+end
