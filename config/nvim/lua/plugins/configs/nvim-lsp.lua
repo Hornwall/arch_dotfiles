efbc76025fc5785558daa2b12fef406fab2fb2da
Hannes Hornwall <hannes@hornwall.me>
2022-10-22 21:12:05 +0200

    Update dotfiles

--- config/nvim/lua/plugins/configs/nvim-lsp.lua
+++ config/nvim/lua/plugins/configs/nvim-lsp.lua
@@ -1,4 +1,4 @@
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 function map_keys()
   vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
